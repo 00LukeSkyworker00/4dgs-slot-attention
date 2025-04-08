5e76532b51aec48f8f81df1d71643bb9830f8857
skyworker-sinica <luke.yc.yang@gmail.com>
2025-04-08 14:49:24 +0800

    change variable names

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NAME=movi_a_test_0408_rawGs_xyz_col
+NAME=movi_a_test_0408_rawGs_xyz_col_15slots
 DATA_DIR=/home/skyworker/result/4DGS_SlotAttention/shape_of_motion
 OUT_DIR=/home/skyworker/result/4DGS_SlotAttention/slot_4dgs
 
 python train.py \
     --data_dir $DATA_DIR \
     --output_dir $OUT_DIR/$NAME \
-    --cfg ./configs/raw_gs.yaml
+    --cfg ./configs/15_slots.yaml
